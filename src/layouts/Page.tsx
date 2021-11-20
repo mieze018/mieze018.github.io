bf1a7c9f42c6bb395a86fbbcab0ccad8934950f7
nakata <mieze018@users.noreply.github.com>
2021-11-20 20:49:11 +0900

    🧰chore:  react-helmet

--- src/layouts/Page.tsx
+++ src/layouts/Page.tsx
@@ -1,4 +1,5 @@
 import React, { forwardRef } from 'react';
+import { Helmet } from 'react-helmet';
 
 type DivProps = React.HTMLProps<HTMLDivElement>;
 
@@ -6,6 +7,11 @@ const Page = forwardRef<HTMLDivElement, DivProps>(
   ({ children, title = '', ...rest }, ref) => {
     return (
       <div ref={ref} {...rest}>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>My Title</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
         {children}
       </div>
     );
