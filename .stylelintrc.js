723f520f78bd9f622b77571d9b5124b7aff07f79
nakata <mieze018@users.noreply.github.com>
2021-11-23 16:09:11 +0900

    🧰chore: stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,15 @@
+﻿module.exports = {
+  plugins: ['stylelint-prettier'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-prettier/recommended',
+    'stylelint-config-prettier'
+  ],
+  rules: {
+    'comment-empty-line-before': 'never',
+    'rule-empty-line-before': 'never'
+  },
+  ignoreShorthands: ['/flex/'],
+  ignoreFiles: ['**/node_modules/**']
+};
