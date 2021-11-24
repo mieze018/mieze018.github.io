c024dc1bf95660232baf2a1f9a77916411742d00
nakata <mieze018@users.noreply.github.com>
2021-11-24 15:41:29 +0900

    💡refactor: スティッキーヘッダー改良
    fixedにしてブレないようにした

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      minHeight: {
-        golden16v: '16.18vh',
-        golden61v: '61.8vh',
-        '92vh': '92vh',
-        square: 'calc((92vh + 92vw) /2)'
-      },
-      minWidth: {
-        golden61v: '61.8vw',
-        golden38v: '38.2vw',
-        golden23v: '23.6vw'
-      },
-      maxWidth: {
+      spacing: {
         golden61v: '61.8vw',
         golden38v: '38.2vw',
         golden23v: '23.6vw'
