115bbda7134663807ee119f0968461365d010876
nakata <mieze018@users.noreply.github.com>
2021-11-24 15:50:21 +0900

    🔧fix:tailwindの設定間違えて消しちゃったの戻し

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      minHeight: {
+        square: 'calc((92vh + 92vw) /2)',
+        golden61v: '61.8vw',
+        golden38v: '38.2vw',
+        golden23v: '23.6vw'
+      },
       spacing: {
+        square: 'calc((92vh + 92vw) /2)',
         golden61v: '61.8vw',
         golden38v: '38.2vw',
         golden23v: '23.6vw'
