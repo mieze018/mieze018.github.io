f879195771720bea4365ae9db264107a2483bf2e
nakata <mieze018@users.noreply.github.com>
2021-11-27 14:34:48 +0900

    🧹clean: 未使用のCSSを削除したりtailwindのテーマに移したりした

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,59 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    textColor: {
+      primary: '#555',
+      secondary: '#797979',
+      darkest: '#222'
+    },
+    fontFamily: {
+      kakugo: [
+        'Hiragino Kaku Gothic ProN',
+        'ヒラギノ角ゴ ProN W3',
+        'Meiryo',
+        'メイリオ',
+        'ＭＳ ゴシック',
+        'MS Gothic',
+        'sans-serif'
+      ],
+      meiryo: [
+        'Meiryo',
+        'メイリオ',
+        'Hiragino Kaku Gothic ProN',
+        'ヒラギノ角ゴ ProN W3',
+        'ＭＳ ゴシック',
+        'MS Gothic',
+        'sans-serif'
+      ],
+      mincho: [
+        'HGS明朝E',
+        'ヒラギノ明朝 Pro W3',
+        'Hiragino Mincho Pro',
+        'ＭＳ Ｐ明朝',
+        'serif'
+      ],
+      serif: [
+        'Baskerville Old Face',
+        'Baskerville',
+        'Times New Roman',
+        'Times',
+        'Georgia',
+        'Palatino',
+        'Palatino Linotype',
+        'HGS明朝E',
+        'ヒラギノ明朝 Pro W3',
+        'Hiragino Mincho Pro',
+        'ＭＳ Ｐ明朝',
+        '游明朝',
+        'YuMincho',
+        'ヒラギノ明朝 ProN W3',
+        'Hiragino Mincho ProN',
+        'MS PMincho',
+        'serif'
+      ],
+      sans: ['Merriweather', 'serif']
+    },
+
     extend: {
       minHeight: {
         square: 'calc((92vh + 92vw) /2)',
@@ -30,6 +83,9 @@ module.exports = {
           'transition-timing-function': 'ease-in-out',
           'transition-duration': '2000ms'
         }
+      },
+      letterSpacing: {
+        title: '0.2em'
       }
     }
   },
