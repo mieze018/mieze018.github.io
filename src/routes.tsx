becc76c7267084ffd820782d57b82b1c12e201b4
nakata <mieze018@users.noreply.github.com>
2021-12-01 01:22:59 +0900

    💡refactor: ルーターのパスを設定した

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,22 +1,47 @@
 import { Navigate } from 'react-router-dom';
 import MainLayout from 'layouts/MainLayout';
 import IndexView from 'views/Index';
+import CommissionedWorkView from 'views/CommissionedWork';
+import PersonalWorkView from 'views/PersonalWorkView';
 //"View"で終わってる必要がある
-export const urls = {
+export const urls: {
+  [key: string]: {
+    [key: string]: { path: string; element: JSX.Element; title?: string };
+  };
+} = {
   root: {
     index: {
       path: '',
-      element: <IndexView />,
-      title: ' '
+      element: <IndexView />
     },
-    git_repo: {
-      path: process.env.REACT_APP_github_repo_name,
-      element: <IndexView />,
-      title: ' '
+    personal_work: {
+      path: 'personal_work',
+      element: <PersonalWorkView />,
+      title: 'personal work'
     },
+    commissioned_work: {
+      path: 'commissioned_work',
+      element: <CommissionedWorkView />,
+      title: 'commissioned work'
+    },
+    info: {
+      path: 'info',
+      element: <CommissionedWorkView />,
+      title: 'info'
+    },
+    // git_repo: {
+    //   path: process.env.REACT_APP_github_repo_name,
+    //   element: <IndexView />,
+    //   title: ' '
+    // },
     NotFound: { path: '404', element: <IndexView /> }
   }
 };
+export function GetUrl(
+  key: [key0: keyof typeof urls, key1: keyof typeof urls.root]
+): string {
+  return `/${[urls[key[0]][key[1]].path]}/`;
+}
 const routes = [
   {
     path: '/',
