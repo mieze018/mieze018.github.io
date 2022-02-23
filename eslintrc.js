bfced5a9eb6f3b06bb77b0e32b7a1ef548f8616f
Ayu Nakata <mieze018@users.noreply.github.com>
2022-02-24 04:35:43 +0900

    設定変更

--- eslintrc.js
+++ eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
-    'prettier/@typescript-eslint'
+    'prettier/@typescript-eslint',
+    'react-app',
+    'react-app/jest',
+    'airbnb',
+    'airbnb/hooks',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { project: ['./tsconfig.json'] },
@@ -16,7 +20,16 @@ module.exports = {
         allowString: false,
         allowNumber: false
       }
-    ]
+    ],
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'any',
+        prev: 'export',
+        next: 'export',
+      },
+    ],
   },
   ignorePatterns: ['src/**/*.test.ts', 'src/frontend/generated/*']
 };
