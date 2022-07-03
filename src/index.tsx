d00d3053b57a48fb2648ccbae673bbf6a7836788
Ayu Nakata <mieze018@users.noreply.github.com>
2022-07-03 14:17:02 +0900

    rootの書き方をReact18に合わせた

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,23 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import GlobalStyles from 'styles/GlobalStyles';
-// 🧩
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// 🏁
-ReactDOM.render(
-  <React.StrictMode>
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
     <BrowserRouter>
       <GlobalStyles />
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    </BrowserRouter>);
+}
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
